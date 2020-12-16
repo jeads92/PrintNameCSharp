3c5485f3e80977c84a67e27aaf7ad254c69f4485
jeads <60721703+jeads92@users.noreply.github.com>
2020-12-15 20:44:59 -0500

    Uploaded a comment describing what data type the variable holds.

--- PrintName/Program.cs
+++ PrintName/Program.cs
@@ -7,6 +7,7 @@ namespace PrintName
         static void Main(string[] args)
         {
             Console.WriteLine("What is your name? ");
+            // userName is a string that holds the name of the user.
             string userName = Console.ReadLine();
             Console.WriteLine($"Hello, {userName}");
             Console.ReadKey();
