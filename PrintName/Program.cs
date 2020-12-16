8ea8c2780a7b05038fb6158753cf6364adb537b0
jeads <60721703+jeads92@users.noreply.github.com>
2020-12-15 20:33:55 -0500

    Initial Commit

---
+++ PrintName/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrintName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name? ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}");
+            Console.ReadKey();
+        }
+    }
+}
